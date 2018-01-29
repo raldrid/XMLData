59515f0705c1e9dee256e0703add0056d66356ab
RayAldridge <raldridge@csa1.com>
2018-01-29 10:42:42 -0600

    Slideshow from XML Data

---
+++ XMLData/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using XMLData.Models;
+
+namespace XMLData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int id = 1)
+        {
+            ViewBag.Title = "Santa Catalina Mountains, Tucson, AZ";
+            List<Photos> photos = new List<Photos>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Server.MapPath("~/App_Data/SantaCatalinaPhotos.xml"));
+            foreach(XmlNode node in doc.SelectNodes("/photos/photo"))
+            {
+                photos.Add(new Photos
+                {
+                    PhotoId = int.Parse(node["id"].InnerText),
+                    File = node["file"].InnerText, 
+                    Caption = node["caption"].InnerText,
+                    TCaption = node["tcaption"].InnerText,
+                    Date = node["date"].InnerText,
+                    TFile = node["tfile"].InnerText 
+                });
+            }
+            ViewBag.PhotoCount = photos.Count;
+            return View(photos.Where(p => p.PhotoId == id));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ XMLData/Models/Photos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace XMLData.Models
+{
+    public class Photos
+    {
+
+    [Key]
+    public int PhotoId { get; set; }
+    public string File { get; set; }
+    public string Caption { get; set; }
+    public string TCaption { get; set; }
+    public string Date { get; set; }
+    public string TFile { get; set; }
+    
+
+    }
+}
