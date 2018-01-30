8a64e4f66b9223b8434ef2bb9e08917c6e64609d
RayAldridge <raldridge@csa1.com>
2018-01-30 08:10:04 -0600

    Second pull

--- XMLData/Controllers/HomeController.cs
+++ XMLData/Controllers/HomeController.cs
@@ -45,5 +45,27 @@ namespace XMLData.Controllers
 
             return View();
         }
+
+        public ActionResult PhotoIndex()
+        {
+            ViewBag.Title = "Santa Catalina Mountains, Tucson, AZ";
+            List<Photos> photos = new List<Photos>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Server.MapPath("~/App_Data/SantaCatalinaPhotos.xml"));
+            foreach (XmlNode node in doc.SelectNodes("/photos/photo"))
+            {
+                photos.Add(new Photos
+                {
+                    PhotoId = int.Parse(node["id"].InnerText),
+                    File = node["file"].InnerText,
+                    Caption = node["caption"].InnerText,
+                    TCaption = node["tcaption"].InnerText,
+                    Date = node["date"].InnerText,
+                    TFile = node["tfile"].InnerText
+                });
+            }
+ 
+            return View(photos);
+        }
     }
 }
